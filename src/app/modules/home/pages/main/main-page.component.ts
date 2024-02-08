53c3886740da3c537a55435a567b6b1b5d5eec85
Wardenbug <geanjrey@gmail.com>
2024-02-08 17:09:48 +0200

    add simple url pagination at home page

--- src/app/modules/home/pages/main/main-page.component.ts
+++ src/app/modules/home/pages/main/main-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ArtworksService } from '../../services/artworks.service';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 interface Pagination {
   current_page: number;
@@ -22,28 +23,66 @@ export class MainPageComponent implements OnInit {
     query: new FormControl<string>(''),
   });
 
+  private page: number = 1;
+  private query: string = '';
+
   public pagination: Pagination | null = null;
 
-  constructor(private artworkService: ArtworksService) {}
+  constructor(
+    private artworkService: ArtworksService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
   ngOnInit(): void {
-    this.artworkService.getArtworks().subscribe((el: any) => {
-      this.pagination = el.pagination;
-      this.items = el.data;
-      console.log(el);
+    this.route.queryParams.subscribe((params: any) => {
+      console.log(params.q, params.page);
+      if (!params.q && params.page) {
+        this.artworkService
+          .getArtworks({ page: params.page })
+          .subscribe((el: any) => {
+            this.pagination = el.pagination;
+            this.items = el.data;
+            this.page = params.page;
+          });
+      } else {
+        this.artworkService
+          .getArtworksBySearch({ q: params.q, page: params.page })
+          .subscribe((el: any) => {
+            this.pagination = el.pagination;
+            this.items = el.data;
+            this.query = params.q;
+            this.page = params.page;
+            this.updateQueryParams();
+          });
+      }
     });
   }
 
   submitForm() {
-    console.log('submit form');
-    console.log(this.searchForm.value);
-
     this.artworkService
       .getArtworksBySearch({ q: this.searchForm.value.query })
       .subscribe((el: any) => {
-        console.log(el);
         this.pagination = el.pagination;
         this.items = el.data;
+        this.query = String(this.searchForm.value.query);
+        this.page = 1;
+        this.updateQueryParams();
       });
+  }
+
+  private updateQueryParams() {
+    const queryParams = { page: this.page, q: this.query };
+
+    const mergedQueryParams = {
+      ...this.route.snapshot.queryParams,
+      ...queryParams,
+    };
+
+    this.router.navigate([], {
+      queryParams: mergedQueryParams,
+      queryParamsHandling: 'merge',
+    });
   }
 
   public changePage(page: number) {
@@ -55,8 +94,8 @@ export class MainPageComponent implements OnInit {
       .subscribe((el: any) => {
         this.pagination = el.pagination;
         this.items = el.data;
-        console.log(el);
+        this.page = page;
+        this.updateQueryParams();
       });
-    console.log(page);
   }
 }
